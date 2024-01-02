52355488243d7f2c88e4961007a507134de57521
Jesús Guijarro <72225659+JesusEduardoGuijarroSaldana@users.noreply.github.com>
2024-01-02 12:48:21 -0600

    :construction: fixing communication throw render.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routesUser = require("./src/routes/auth.routes");
+const cors = require("cors"); // Import the cors middleware
 const app = express();
 const puerto = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 app.use("/socios/v1/users", routesUser);
 const server = app.listen(puerto, () =>
-	console.log(`Server Connected to port ${puerto}`)
+  console.log(`Server Connected to port ${puerto}`)
 );
